b722b8076bf4f8b1cb7aa4dfb98d98ce781067e6
Zlatil Veselinov Zlatilov <zlatilveselinovzlatilov@gmail.com>
2013-04-30 00:40:39 +0300

    Style-formatting

--- Game-Fifteen-4/Scores.cs
+++ Game-Fifteen-4/Scores.cs
@@ -8,65 +8,36 @@
         public Scores(string name, int score)
         {
             this.name = name;
-
-
             this.score = score;
-        
         }
 
         public string Name
-        
         {
-        
-            get
-            
+            get   
             {
-            
                 return name;
             }
 
-            
             set
-            
             {
-            
                 name = value;
             }
-
         }
 
-
-
-        
         public int Score
-        
-        
         {
-        
-            
-            
-            
-            
             get
             {
             
                 return score;
             }
-
-            
+   
             set
-            
             {
-            
                 score = value;
             }
-
-
         }
-
     }
-
-
 }
 
 
