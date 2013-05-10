a507028b1b163a39527a78daf198bc089113bb7a
Radoslav Sholev <radosholev@gmail.com>
2013-05-10 14:32:50 +0300

    Added project Game15Test
    Createt class ConsoleRendererTest
    Createt test method TestRenderMessage();

---
+++ Game15Test/ConsoleRendererTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GameFifteen;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Game15Test
+{
+    [TestClass]
+    public class ConsoleRendererTest
+    {
+        [TestMethod]
+        public void TestRenderMessage()
+        {
+            string message = "This is some message.";
+            ConsoleRenderer renderer = new ConsoleRenderer();
+            renderer.RenderMessage(message);
+        }
+    }
+}
