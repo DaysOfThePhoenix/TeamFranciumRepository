2bcd748928efb64c76ac52712d84a91fc93b38d0
Radoslav Sholev <radosholev@gmail.com>
2013-05-13 17:54:16 +0300

    Added TestEngine class

---
+++ Game15Test/TestEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameFifteen;
+
+namespace Game15Test
+{
+    [TestClass]
+    public class TestEngine
+    {
+        [TestMethod]
+        public void TestLoadBoard()
+        {
+            Board comparingBoard = new Board(4, 4);
+            Board engineBoard = new Board(4, 4);
+            Engine testEngine = new Engine(null, engineBoard, null);
+            comparingBoard.InitializeMatrix();
+            testEngine.LoadBoard();
+
+            int differences = 0;
+            int MinCountOfDifferences = 
+                (comparingBoard.MatrixSizeRows * comparingBoard.MatrixSizeColumns) / 2;
+
+            for (int row = 0; row < comparingBoard.MatrixSizeRows; row++)
+            {
+                for (int col = 0; col < comparingBoard.MatrixSizeColumns; col++)
+                {
+                    if (comparingBoard.Matrix[row,col] != engineBoard.Matrix[row,col])
+                    {
+                        differences++;
+                    }
+                }
+            }
+
+            bool areEqual = true;
+            if (differences > MinCountOfDifferences)
+            {
+                areEqual = false;
+            }
+
+            Assert.IsFalse(areEqual);
+        }
+    }
+}
