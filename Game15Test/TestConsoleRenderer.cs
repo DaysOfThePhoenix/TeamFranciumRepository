5a2ae1a905197a75693efe39611649a0d14c9a18
Radoslav Sholev <radosholev@gmail.com>
2013-05-13 16:32:08 +0300

    Added unit test for rendering matrix in TestConsoleRenderer class

--- Game15Test/TestConsoleRenderer.cs
+++ Game15Test/TestConsoleRenderer.cs
@@ -25,5 +25,30 @@ namespace Game15Test
 
             Assert.AreEqual(expected.ToString(), writer.ToString());
         }
+
+        [TestMethod]
+        public void TestRenderMatrix()
+        {
+            ConsoleRenderer renderer = new ConsoleRenderer();
+            Board gameBoard = new Board(4, 4);
+            gameBoard.InitializeMatrix();
+
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+            using (writer)
+            {
+                renderer.RenderMatrix(gameBoard);
+            }
+
+            StringBuilder expected = new StringBuilder();
+            expected.AppendLine("  ------------- ");
+            expected.AppendLine(" |  1  2  3  4 |");
+            expected.AppendLine(" |  5  6  7  8 |");
+            expected.AppendLine(" |  9 10 11 12 |");
+            expected.AppendLine(" | 13 14 15    |");
+            expected.AppendLine("  ------------- ");
+
+            Assert.AreEqual(expected.ToString(), writer.ToString());
+        }
     }
 }
