d4bda38bb72e24f91d169c116e09ff035aab2b48
Radoslav Sholev <radosholev@gmail.com>
2013-05-13 15:44:30 +0300

    Added TestConsoleRenderer unit test.
    Createt test method for RenderMessage

---
+++ Game15Test/TestConsoleRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using GameFifteen;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+
+namespace Game15Test
+{
+    [TestClass]
+    public class TestConsoleRenderer
+    {
+        [TestMethod]
+        public void TestRenderMessage()
+        {
+            ConsoleRenderer renderer = new ConsoleRenderer();
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+            using (writer)
+            {
+                renderer.RenderMessage(Messages.GetGoodbye());
+            }
+
+            StringBuilder expected = new StringBuilder();
+            expected.AppendLine("Good bye!");
+
+            Assert.AreEqual(expected.ToString(), writer.ToString());
+        }
+    }
+}
