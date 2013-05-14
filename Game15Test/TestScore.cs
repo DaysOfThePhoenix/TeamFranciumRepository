46dfa96c3af49b9a9ac6d04c75083a1b378cd6f3
DaysOfThePhoenix <zlatilveselinovzlatilov@gmail.com>
2013-05-14 14:30:17 -0700

    Updated method for testing initialization of class member with valid parameters

--- Game15Test/TestScore.cs
+++ Game15Test/TestScore.cs
@@ -12,6 +12,9 @@ namespace Game15Test
         {
             Score scoreForTest = new Score("testName", 200, 4, "testFileName.txt");
             Assert.AreEqual(scoreForTest.Name, "testName");
+            Assert.AreEqual(scoreForTest.Points, 200);
+            Assert.AreEqual(scoreForTest.TopScoresCount, 4);
+            Assert.AreEqual(scoreForTest.FileNameForExternalSave, "testFileName.txt");
         }
     }
 }
